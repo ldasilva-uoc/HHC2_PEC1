6bfe2d78b23b0935277159b5b94f35a8646a3383
Luiza Clara Da Silva <ldasilva@uoc.edu>
2021-10-14 23:42:52 +0200

    CSS protada

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -14,3 +14,27 @@
   const university = "UOC";
   console.log(`Hello, ${university}!`);
 } )();
+
+const sections = document.querySelectorAll('section');
+const navLi = document.querySelectorAll('.header__nav__list-item')
+
+window.addEventListener('scroll', ()=> {
+  let current = '';
+
+  sections.forEach( section  => {
+    const sectionTop = section.offsetTop;
+    const sectionHeight = section.clientHeight;
+
+    if(pageYOffset >= sectionTop)
+    {
+      current = section.getAttribute('id')
+    }
+  })
+
+  navLi.forEach( li => {
+    li.classList.remove('header__nav__list-item--selected');
+    if(li.classList.contains(current)){
+      li.classList.add('header__nav__list-item--selected');
+    }
+  })
+} )
